1bcf0f153977caad7eec01c9e9b85150e574f68e
windwesd <hjkims1122@naver.com>
2024-09-03 17:11:40 +0900

    20240903_Spring 커밋

---
+++ demoProject1/src/main/java/edu/kh/demo/DemoProject1Application.java
@@ -0,0 +1,15 @@
+package edu.kh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// OOOAplication.java 파일
+// - 프로젝트 생성 시 자동으로 생성되는 Spring Boot 프로젝트 실행 파일
+@SpringBootApplication
+public class DemoProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject1Application.class, args);
+	}
+
+}
